f980d4a974d6252c1eeef2253fcc85f180210c70
Mathieu Rousse <mathieu@rousse.me>
2016-05-22 12:37:27 +0200

    Ajout de contenu pour le minitel + tout le javascript (liens, images, toussa toussa)

--- js/minitel.js
+++ js/minitel.js
@@ -1,18 +1,59 @@
 var minitelBoot = false;
 
+function typeProject(project){
+  $("#minitel p.miniteltyped").typed({
+    strings: ['<p><span class="PS1"><span class="PS1user">root</span>@<span class="PS1server">minitel</span>:<span class="PS1path">/root</span># </span>cd Projects</p>'
+    +'<p><span class="PS1"><span class="PS1user">root</span>@<span class="PS1server">minitel</span>:<span class="PS1path">/root/Projects</span># </span>explain '+project+'</p>'],
+    typeSpeed: 2,
+    contentType: 'html',
+    callback: function(){
+      var explProj = "#explain".concat(project);
+      $(explProj).clone().appendTo("#minitel p.miniteltyped");
+    }
+  });
+}
+
+function checkMinitel(e){
+  if(minitelBoot){
+    $("#minitel p.miniteltyped").typed('reset');
+    $("#minitel").empty();
+    $("#minitel").append($("<p class='miniteltyped'></p>"));
+  }else{
+    command(e, "getMinitel");
+  }
+}
+
 function command(e, cmd, opt=""){
   e.preventDefault();
-  console.log(cmd);
   switch(cmd){
     case "getMinitel":
-      console.log("Minitel is booting");
       if(minitelBoot){
         $("#left-pane").removeClass("left-logo-upleft");
+        $("#minitel").addClass("minitel-out");
+        $("#minitel p.miniteltyped").typed('reset');
+        $("#minitel").empty();
+        $("#minitel").append($("<p class='miniteltyped'></p>"));
         minitelBoot = false;
       }else{
         $("#left-pane").addClass("left-logo-upleft");
+        $("#minitel").removeClass("minitel-out");
         minitelBoot = true;
       }
+      break;
+    case "getProject":
+      checkMinitel(e);
+      typeProject(opt);
+      break;
+    case "getHardware":
+      checkMinitel(e);
+      $("#minitel p.miniteltyped").typed({
+        strings: ['<p><span class="PS1"><span class="PS1user">root</span>@<span class="PS1server">minitel</span>:<span class="PS1path">/root</span># </span>'+cmd+' '+ opt +'</p>'],
+        typeSpeed: 2,
+        contentType: 'html',
+        callback: function(){
+          $("#explainHardware").clone().appendTo("#minitel p.miniteltyped");
+        }
+      });
       break;
   }
 };
